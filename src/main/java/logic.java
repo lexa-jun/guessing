6a759b53e750cf8cc07e9f9ab3e8009e71110567
Diana <basss_1993@list.ru>
2021-04-10 22:30:16 +0500

    logic creation
    Создание (классов Player , logic);
    Создание (переменные экземпляр Player);

--- src/main/java/logic.java
+++ src/main/java/logic.java
@@ -1,2 +1,14 @@
 public class logic {
+    Player pl;
+    Player pl1;
+    Player pl2;
+
+    public void startGame(){
+        pl = new Player();
+        pl1 = new Player();
+        pl2 = new Player();
+
+
+    }
+
 }
